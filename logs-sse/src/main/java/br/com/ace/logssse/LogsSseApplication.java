b1e0afbcdf1a5f365ed5e763f34541ea9d4478a9
Ace0777 <luishhasantos@gmail.com>
2025-08-30 19:32:05 -0300

    chore: create project

---
+++ logs-sse/src/main/java/br/com/ace/logssse/LogsSseApplication.java
@@ -0,0 +1,13 @@
+package br.com.ace.logssse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsSseApplication.class, args);
+	}
+
+}
---
+++ logs-sse/src/test/java/br/com/ace/logssse/LogsSseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ace.logssse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsSseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
